2e3b4aa9cf9ac9cdd51ffc2edaa716cc6e9b8ce1
Revolt <revoltsgfx@gmail.com>
2024-10-21 10:02:20 -0400

    before final

--- src/main/java/lol/revolt/petpassiveregen/mixin/ExampleMixin.java
+++ src/main/java/lol/revolt/petpassiveregen/mixin/ExampleMixin.java
@@ -1,14 +1,21 @@
 package lol.revolt.petpassiveregen.mixin;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.entity.passive.HorseEntity;
 import net.minecraft.entity.passive.OcelotEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.MinecraftServer;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
@@ -17,20 +24,31 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import lol.revolt.petpassiveregen.test;
 import net.minecraft.util.math.random.Random;
 
 @Mixin(AnimalEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "tickMovement")
+	@Inject(at = @At("TAIL"), method = "tickMovement")
 
 	private void injectHealing(CallbackInfo info) {
 		AnimalEntity entity = (AnimalEntity) (Object) this;
 		if (entity.getHealth() >= entity.getMaxHealth())
+			return;
+
+		// Rideable tames automatically have regeneration
+		// includes horses, donkeys, mules, skeleton horses and llamas
+		// all these are based on AbstractHorseEntity
+		if (entity instanceof AbstractHorseEntity)
 			return;
 		if (entity instanceof TameableEntity) {
 			TameableEntity tameableEntity = (TameableEntity) (Object) entity;
@@ -60,30 +78,28 @@ public class ExampleMixin {
 	}
 
 	private void handleOcelotEntity(OcelotEntity self) {
-		// boolean isTrusting = ((OcelotEntityAccessor) self).invokeIsTrusting();
-		// NbtCompound nbt = new NbtCompound();
-		// self.writeNbt(nbt);
-		// boolean isTrusting = nbt.getBoolean("Trusting");
-		if (!self.getWorld().isClient && self.isAlive() && self.isTrusting()) {
-			attemptHeal(self);
-		}
+		if (!self.getWorld().isClient && self.isAlive()) {
+			boolean isTrusting = ((OcelotEntityAccessor) self).invokeIsTrusting();
+            //boolean isTrusting = self.isTrusting();
+            if (!isTrusting) return;
+            attemptHeal(self);
+        }
 	}
 
 	private void handleFoxEntity(FoxEntity self) {
 		List<UUID> uuids = ((FoxEntityAccessor) self).invokeGetTrustedUuids();
-		if (uuids.isEmpty()) {
-			// System.out.println("not healing fox");
-			return;
-		}
-		if (!self.getWorld().isClient && self.isAlive()) {
+		
+		if (!self.getWorld().isClient && self.isAlive() && !uuids.isEmpty()) {
 			attemptHeal(self);
 		}
 	}
 
+	@Unique
 	private void attemptHeal(AnimalEntity entity) {
-		if (entity.getRandom().nextInt(900) == 0 && entity.deathTime == 0) {
-			entity.heal(1.0F);
-			System.out.println("healing: " + entity.getType());
-		}
+		System.out.println("Attempting to heal: " + entity.getType());
+    if (entity.getRandom().nextInt(900) == 0 && entity.deathTime == 0) {
+        entity.heal(1.0F);
+        System.out.println("Healed: " + entity.getType());
+    }
 	}
 }
--- src/main/java/lol/revolt/petpassiveregen/mixin/FoxEntityAccessor.java
+++ src/main/java/lol/revolt/petpassiveregen/mixin/FoxEntityAccessor.java
@@ -1,18 +1,11 @@
 package lol.revolt.petpassiveregen.mixin;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
-
-import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.passive.FoxEntity;
-
-import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(FoxEntity.class)
 public interface FoxEntityAccessor {
